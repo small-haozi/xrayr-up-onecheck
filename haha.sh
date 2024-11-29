d8affb694735a0647f69ac3001de87f56b3e8e49
small-haozi <165977055+small-haozi@users.noreply.github.com>
2024-11-30 03:52:00 +0800

    Update haha.sh

--- haha.sh
+++ haha.sh
@@ -42,7 +42,7 @@ echo "NODE_ID=$NODE_ID" > "$PARAM_FILE"
 echo "NODE_TYPE=$NODE_TYPE" >> "$PARAM_FILE"
 echo "UNLOCK_SERVICES=$UNLOCK_SERVICES" >> "$PARAM_FILE"
 
-echo "${YELLOW}最近一次的参数已保存到${GREEN} /etc/xrayr-onecheck/last_params.txt ${YELLOW}中，可查看最近一次的配置项${NC}"
+echo -e "${YELLOW}最近一次的参数已保存到${GREEN} /etc/xrayr-onecheck/last_params.txt ${YELLOW}中，可查看最近一次的配置项${NC}"
 
 # 备份原文件
 echo -e "${BLUE}备份原配置文件...${NC}"
